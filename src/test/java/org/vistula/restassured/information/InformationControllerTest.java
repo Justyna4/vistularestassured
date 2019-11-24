7c645c429457e2528c3a507b7f4fc587f046b776
Justyna4 <justa21dom@interia.eu>
2019-11-24 16:49:23 +0100

    Zajęcia 24/11

--- src/test/java/org/vistula/restassured/information/InformationControllerTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerTest.java
@@ -1,10 +1,17 @@
 package org.vistula.restassured.information;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
 
 public class InformationControllerTest extends RestAssuredTest {
 
@@ -16,5 +23,48 @@ public class InformationControllerTest extends RestAssuredTest {
                 .statusCode(200)
                 .body("size()", is(2));
     }
+
+    @Test
+    public void shouldCreateNewInfo() {
+
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(7);
+        String nation = "Polish";
+        int mySalary = 100000;
+
+        requestParams.put("id", 6);
+        requestParams.put("name", myName);
+        requestParams.put("nationality", nation);
+        requestParams.put("salary", mySalary);
+
+       given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/information")
+                .then()
+                .log().all()
+                .statusCode(201)
+
+                .body("name", equalTo(myName))
+                .body("nationality", equalTo(nation))
+                .body("salary", equalTo(mySalary))
+                .body("id", greaterThan(0));
+    }
+
+    @Test
+    public void shouldDeleteFirstInfo() {
+        given().delete("/information/1")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void shouldDeleteTenthInfo() {
+        given().delete("/information/10")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
 
 }
--- src/test/java/org/vistula/restassured/pet/PetControllerTest.java
+++ src/test/java/org/vistula/restassured/pet/PetControllerTest.java
@@ -1,10 +1,15 @@
 package org.vistula.restassured.pet;
 
+import io.restassured.internal.common.assertion.Assertion;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -30,10 +35,53 @@ public class PetControllerTest extends RestAssuredTest {
     }
 
     @Test
+    public void shouldGetSecondPet() {
+        Object name = given().get("/pet/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Dog"))
+                .extract().path("name");
+
+            assertThat(name).isEqualTo("Dog");
+
+    }
+
+    @Test
+    public void shouldGetSecondsPet() {
+        Pet pet = given().get("/pet/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Dog"))
+                .extract().body().as(Pet.class);
+
+        assertThat(pet.getId()).isEqualTo(2);
+        assertThat(pet.getName()).isEqualTo("Dog");
+
+    }
+
+    @Test
+    public void shouldntGetSecondPet() {
+        int statusCode = given().get("/pet/9")
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body(equalTo("There is not Pet with such id"))
+                .extract().statusCode();
+
+            assertThat(statusCode).isEqualTo(404);
+    }
+
+
+    @Test
     public void shouldCreateNewPet() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("id", 4);
-        requestParams.put("name", "Wolf");
+        int value = ThreadLocalRandom.current().nextInt(20,Integer.MAX_VALUE);
+        requestParams.put("id", value);
+        requestParams.put("name", RandomStringUtils.randomAlphabetic(10));
 
         given().header("Content-Type", "application/json")
                 .body(requestParams.toString())
@@ -41,6 +89,18 @@ public class PetControllerTest extends RestAssuredTest {
                 .then()
                 .log().all()
                 .statusCode(201);
+        given().delete("/pet/" + value)
+                .then()
+                .log().all()
+                .statusCode(204);
     }
 
+    @Test
+    public void shouldDeleteFourthPet() {
+        given().delete("/pet/4")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
 }
+
